4d276644efe2761dbf5f22c16209e735249b5583
emendezduran <emendezduran@nautilus25.danielcastelao.org>
2018-11-21 13:46:37 +0100

    commit

--- aplicacion/Aplicacion.java
+++ aplicacion/Aplicacion.java
@@ -1,5 +1,7 @@
 package aplicacion;
 
+import java.util.Scanner;
+
 /**
  *
  * @author emendezduran
@@ -7,15 +9,20 @@ package aplicacion;
 public class Aplicacion {
 
       public static void main(String[] args) {
+          System.out.println("Introduce los datos solicitados a continuacion:");
+          Scanner dato = new Scanner(System.in);
+          System.out.println("Nucleos y Ram");
+          Cpu processor = new Cpu(dato.nextInt(), dato.nextInt());
+          System.out.println("Inhalambrico(True/False) y Modelo");
+          Raton mouse = new Raton(dato.nextBoolean(), dato.next());
+          System.out.println("Numero de Teclas");
+          Teclado keyboard = new Teclado(dato.nextInt());
+          System.out.println("Marca del Monitor y Pulgadas");
+          Monitor screen = new Monitor(dato.next(), dato.nextInt());
+          Ordenador computer1 = new Ordenador(processor, mouse, keyboard, screen);
+          computer1.visualizarOrdenador();
+          System.out.println("PRECIO: {" + computer1.calcularPrecio() + " €}");
 
-        Cpu processor = new Cpu(4, 8);
-        Raton mouse = new Raton(true, "QWERTY1");
-        Teclado keyboard = new Teclado(101);
-        Monitor screen = new Monitor("AOC", 16);
-        Ordenador computer1 = new Ordenador(processor, mouse, keyboard, screen);
-        computer1.visualizarOrdenador();
-        System.out.println("PRECIO: {" + computer1.calcularPrecio() + " €}");
-        
         
     }
 }
